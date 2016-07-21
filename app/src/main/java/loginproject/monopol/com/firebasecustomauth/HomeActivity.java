082d9e1525fd2c6ff31a8dee56b8c589ffb75d64
Monopoltech <developer@monopolyonetim.com>
2016-07-21 19:02:34 +0300

    Veri listesi geliyor

--- app/src/main/java/loginproject/monopol/com/firebasecustomauth/HomeActivity.java
+++ app/src/main/java/loginproject/monopol/com/firebasecustomauth/HomeActivity.java
@@ -5,22 +5,35 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import loginproject.monopol.com.firebasecustomauth.arca.SessionManager;
 
-
 public class HomeActivity extends AppCompatActivity {
     private SessionManager session;
+
     ImageButton profileBut, addBut, searchBut;
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    public String getItems;
 
-    User user;
+    AddActivity addActivity = new AddActivity();
+
+    public ArrayList<String> arrayList = new ArrayList<String>();
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     DatabaseReference myRef = database.getReference("Users");
     private DatabaseReference mDatabase;
@@ -38,6 +51,7 @@ public class HomeActivity extends AppCompatActivity {
         searchBut = (ImageButton)findViewById(R.id.searchButton);
         liste = (ListView)findViewById(R.id.hashList);
 
+
         profileBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,6 +67,7 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
 
         searchBut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,13 +85,60 @@ public class HomeActivity extends AppCompatActivity {
         session = new SessionManager(getApplicationContext());
         if (session.isUid() ==null){
             Log.d("Arc:","Uid yok");
+            finish();
         }else{
             Log.d("Arc:",session.isUid());
         }
 
+
+        mDatabase.child("Users").child(session.isUid()).child("Hashtag").addValueEventListener(new ValueEventListener() {
+            @Override
+
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("Veriler:",dataSnapshot.getValue()+"");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("Errorlog",databaseError.getMessage());
+
+            }
+        });
+
+        mDatabase.child("Users").child(session.isUid()).child("Hashtag").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Map<String, Object> newPost = (Map<String, Object>)dataSnapshot.getValue();
+                Log.d("Author: ", newPost.get("title").toString());
+                arrayList.add(newPost.get("title").toString());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         //Log.d("veri :",mDatabase.child("Users").child(user.userId).limitToFirst(100).toString());
         //String deger = String.format("%s",user.mDatabase.child("Users").child(user.userId).limitToFirst(100));
         //getItems(deger);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,android.R.id.text1,arrayList);
+        liste.setAdapter(arrayAdapter);
     }
 
 
