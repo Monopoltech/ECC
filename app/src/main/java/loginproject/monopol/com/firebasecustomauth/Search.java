ffa6266d246aac007e47a06121ea0362cb3c1d41
Canan Sayar <canannsayar@gmail.com>
2016-07-22 16:38:36 +0300

    cnan

--- app/src/main/java/loginproject/monopol/com/firebasecustomauth/Search.java
+++ app/src/main/java/loginproject/monopol/com/firebasecustomauth/Search.java
@@ -1,51 +1,74 @@
 package loginproject.monopol.com.firebasecustomauth;
 
-import android.net.Uri;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-public class Search extends AppCompatActivity {
+public class Search extends ListActivity {
 
+
+    private DatabaseReference mDatabase;
+    public ArrayList<String> tagsArray = new ArrayList<String>();
+
+    public void getTags(){
+
+        mDatabase.child("Hashtag").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("veri : ",dataSnapshot.getChildren()+"");
+               /* Map<String, Object> newPost = (Map<String, Object>)dataSnapshot.getValue();
+                Log.d("Author: ", newPost.get("title").toString());
+                tagsArray.add(newPost.get("title").toString());*/
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("Veri Hata search : ",databaseError.getMessage());
+            }
+        });
+    }
     ListView list;
-    Button buton;
-    ImageButton imagebuton;
-    TextView tv;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
+
     private GoogleApiClient client;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+ @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
-//back butonu home adlı sayfaya donucek
-
-        tv = (TextView) findViewById(R.id.textView);
-        imagebuton = (ImageButton) findViewById(R.id.imageButton);
-        buton = (Button) findViewById(R.id.button);
-        list = (ListView) findViewById(R.id.listView);
-
+        mDatabase =  FirebaseDatabase.getInstance().getReference();
+        getTags();
 
     }
+    public void onListItemClick(ListView parent, View v, int position, long id){
+        CheckedTextView item=(CheckedTextView) v;
+        Toast.makeText(this,tagsArray.get(position)+" checked : "+item.isChecked(),Toast.LENGTH_SHORT).show();
 
+    }
 }
+
 
 
